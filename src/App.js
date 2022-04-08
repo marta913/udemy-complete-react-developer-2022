5e56156050d7205eb29327d8620e6c263736139d
Marta Jurčević <marta.raptavi997@gmail.com>
2022-04-08 11:53:49 +0200

    Init commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import Home from "./routes/home/home.component";
+import { Routes, Route } from "react-router-dom";
+import Navigation from "./routes/navigation/navigation.component.jsx";
+
+const Shop = () => {
+  return <h1>I am the shop page</h1>
+};
+
+
+
+const App = () => {
+  return (
+    <Routes>
+      <Route path="/" element={<Navigation />}>
+        <Route index element={<Home />} />
+        <Route path="shop" element={<Shop />} />
+      </Route>
+    </Routes>
+  );
+};
+export default App;
+
+
+
