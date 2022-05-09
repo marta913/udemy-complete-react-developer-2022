084014b313af698f914279f0414cae0bbd50b9e0
Marta Jurčević <marta.raptavi997@gmail.com>
2022-05-09 14:05:05 +0200

    CSS in JS + styled-components (section 11)

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,33 +1,37 @@
-import { useContext } from "react"
-import { useNavigate } from "react-router-dom"
-import { CartContext } from "../../contexts/cart.context"
-import Button from "../button/button.component"
+import { useContext } from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import CartItem from "../cart-item/cart-item.component"
+import { CartContext } from '../../contexts/cart.context';
 
-import "./cart-dropdown.styles.scss"
+import Button from '../button/button.component';
+import CartItem from '../cart-item/cart-item.component';
 
+import {
+  CartDropdownContainer,
+  EmptyMessage,
+  CartItems,
+} from './cart-dropdown.styles';
 
+const CartDropdown = () => {
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
 
-const CartDropDown = () => {
-    const { cartItems } = useContext(CartContext);
-    const navigate = useNavigate();
+  const goToCheckoutHandler = () => {
+    navigate('/checkout');
+  };
 
-    const goToCheckoutHandler =() => {
-        navigate("/checkout")
-    }
+  return (
+    <CartDropdownContainer>
+      <CartItems>
+        {cartItems.length ? (
+          cartItems.map((item) => <CartItem key={item.id} cartItem={item} />)
+        ) : (
+          <EmptyMessage>Your cart is empty</EmptyMessage>
+        )}
+      </CartItems>
+      <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
+    </CartDropdownContainer>
+  );
+};
 
-
-    return (
-        <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {cartItems.map(item => (
-                    <CartItem key={item.id} cartItem={item} />
-                ))}
-            </div>
-            <button onClick={goToCheckoutHandler}>GO TO CHECKOUT</button>
-        </div>
-    )
-}
-
-export default CartDropDown;
+export default CartDropdown;
--- src/components/checkout-item/checkout-item.component.jsx
+++ src/components/checkout-item/checkout-item.component.jsx
@@ -6,11 +6,11 @@ import "./checkout-item.styles.scss"
 
 const CheckoutItem = ({cartItem}) => {
     const { name, imageUrl, price, quantity } = cartItem;
-    const { clearItemFromCart, addItemToCart, removeItemFromCart } = useContext(CartContext);
+    const { clearItemFromCart, addItemToCart, removeItemToCart } = useContext(CartContext);
 
     const clearItemHandler = () => clearItemFromCart(cartItem);
     const addItemHandler = () => addItemToCart(cartItem);
-    const removeItemHandler = () => removeItemFromCart(cartItem);
+    const removeItemHandler = () => removeItemToCart(cartItem);
 
     return (
         <div className="checkout-item-container">
